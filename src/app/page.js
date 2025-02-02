117fa2f670e516ea6ff1ff950d026554ee17230c
Prince <principe3219@icloud.com>
2025-02-02 12:23:41 -0800

    practice

--- src/app/page.js
+++ src/app/page.js
@@ -10,22 +10,33 @@ function Square({ value, onClick }) {
 }
 
 function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function onSquareClick(idx) {
+    if (squares[idx]) return;
+    const newSquares = squares.slice();
+    newSquares[idx] = xIsNext ? "X" : "O";
+    setXIsNext(!xIsNext);
+    setSquares(newSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square value={"X"} onClick={() => onSquareClick()}/>
-        <Square value={"X"} onClick={() => onSquareClick()}/>
-        <Square value={"X"} onClick={() => onSquareClick()}/>
+        <Square value={squares[0]} onClick={() => onSquareClick(0)} />
+        <Square value={squares[1]} onClick={() => onSquareClick(1)} />
+        <Square value={squares[2]} onClick={() => onSquareClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={"X"} onClick={() => onSquareClick()}/>
-        <Square value={"X"} onClick={() => onSquareClick()}/>
-        <Square value={"X"} onClick={() => onSquareClick()}/>
+        <Square value={squares[3]} onClick={() => onSquareClick(3)} />
+        <Square value={squares[4]} onClick={() => onSquareClick(4)} />
+        <Square value={squares[5]} onClick={() => onSquareClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={"X"} onClick={() => onSquareClick()}/>
-        <Square value={"X"} onClick={() => onSquareClick()}/>
-        <Square value={"X"} onClick={() => onSquareClick()}/>
+        <Square value={squares[6]} onClick={() => onSquareClick(6)} />
+        <Square value={squares[7]} onClick={() => onSquareClick(7)} />
+        <Square value={squares[8]} onClick={() => onSquareClick(8)} />
       </div>
     </>
   );
